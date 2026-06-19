7c608065277f5afe5287471d9facc9a6146986e6
jdillon006 <jeffreydillon@gmail.com>
2026-06-19 12:17:43 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 const app = express();
 
 app.use(cors());
